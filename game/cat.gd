db8fac2f5fed4b8d5f7ea76c4d749e1006ed01c6
Anton <anrehelenius@gmail.com>
2019-08-22 10:25:01 +0200

    movement!

--- game/cat.gd
+++ game/cat.gd
@@ -4,8 +4,18 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	velocity.x = 100;
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	velocity = Vector2()
+	if(Input.is_action_just_pressed("moveLeft")):
+		velocity.x -= 100;
+	if(Input.is_action_just_pressed("moveRight")):
+		velocity.x += 100;
+	if(Input.is_action_just_pressed("moveUp")):
+		velocity.y -= 100;
+	if(Input.is_action_just_pressed("moveDown")):
+		velocity.y += 100;
+	
 	velocity = move_and_slide(velocity);
